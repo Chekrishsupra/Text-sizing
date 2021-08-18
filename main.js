8f91f717836c9181a26c5239b42042d0bca4aa3b
Chekrishsuper <78082844+Chekrishsupra@users.noreply.github.com>
2021-08-18 14:45:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
-    video = createCapture(VIDEO);
-    video.size(550,500);
-    
-    canvas = createCanvas(550,400);
-    canvas.position(560,150);
-    
-    poseNet = ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
+video = createCapture(VIDEO);
+video.size(550,500);
+
+canvas = createCanvas(550,400);
+canvas.position(560,150);
+
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function draw() {
+    background('#4d4949');
+    textSize(leftWristX,rightWristX)
+    fill("#f00e0e");
+    text('Chetan',noseX,noseY)
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results); 
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x:" + noseX + ",nose y:" + noseY);
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left wrist x:" + leftWristX + ",Right wrist x:" + rightWristX + "and difference:" + difference);
     }
-    
-    function draw() {
-        background('#4d4949');
-    }
-    
-    function modelLoaded() {
-        console.log("PoseNet is initialized");
-    }
-    
-    function gotPoses(results) {
-        if(results.length > 0) {
-            console.log(results);
-        }
-    }
+}
